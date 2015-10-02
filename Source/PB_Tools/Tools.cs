5852a4eda8eabde1279c5ec4501c5217561be1c8
Mhoram Kerbin <mhoram.kerbin@gmx.de>
2015-10-03 01:14:33 +0200

    Initial Version

---
+++ Source/PB_Tools/Tools.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace PB_Tools
+{
+    
+    // Log the names of all loaded Dll's
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class LogLoadedDllNames : MonoBehaviour
+    {
+        private static bool doneOnce = false;
+        public void Start()
+        {
+            if (doneOnce)
+            {
+                return;
+            }
+            doneOnce = true;
+
+            Debug.Log("LogLoadedDllNames: " + AssemblyLoader.loadedAssemblies.ToList().Select(la => la.dllName).Aggregate((a, b) => a + ", " + b));
+        }
+    }
+}
